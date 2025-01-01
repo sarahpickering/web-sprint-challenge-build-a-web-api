5d65ae55d4eed6a3efb07e4bd178c369a5c19e6f
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-12-31 17:37:15 -0700

    build projects endpoint, :id endpoint, err handling on both

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
-// Write your "projects" router here!
+const router = require("express").Router();
+const Projects = require("./projects-model");
+
+router.get("/", (req, res, next) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      next({ message: "We ran into an error retrieving the projects" });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Projects.get(req.params.id)
+    .then((project) => {
+      if (project) res.status(200).json(project);
+    })
+    .catch((err) => next({ status: 404, message: "Project not found" }));
+});
+
+module.exports = router;
